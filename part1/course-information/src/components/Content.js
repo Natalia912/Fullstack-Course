58c2886ab4431a9b100519af97d1046fe43f577a
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-01-29 15:05:20 +0300

    part1: exercises 1.3 - 1.5

--- part1/course-information/src/components/Content.js
+++ part1/course-information/src/components/Content.js
@@ -1,11 +1,11 @@
 import Part from "./Part";
 
-function Content(props) {
+function Content({parts}) {
   return ( 
     <>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={parts[0].name} exercise={parts[0].exercises} />
+      <Part part={parts[1].name} exercise={parts[1].exercises} />
+      <Part part={parts[2].name} exercise={parts[2].exercises} />
     </>
   );
 }
