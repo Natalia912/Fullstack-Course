a58e4e2bcc267d356c06015bea0c5e0040067cfe
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-02-05 14:24:53 +0300

    part2: exercise 2.1

---
+++ part2/courseinfo/src/components/Content.js
@@ -0,0 +1,17 @@
+import Part from "./Part";
+
+function Content({parts}) {
+
+  return ( 
+    <>
+      {parts.map(part => 
+        <Part 
+          key={part.id} 
+          part={part.name} 
+          exercise={part.exercises} 
+        />)}
+    </>
+  );
+}
+
+export default Content;
