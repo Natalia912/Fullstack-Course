bd1400ff739665a9f73e92db2b740869c056b121
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-02-23 14:28:22 +0300

    part3: exercise 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -45,6 +47,15 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const note = request.body
+
+  const newId = Math.ceil(Math.random()*10000)
+  response.json(note)
+
+  persons = persons.concat({...note, id: newId})
 })
 
 
