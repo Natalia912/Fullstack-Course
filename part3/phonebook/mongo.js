18eef9e2abc8ee5c65205fab2867f6d824c14d95
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-03-04 19:34:27 +0600

    part3: exercise 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+let newName = process.argv[3]
+let newNumber = process.argv[4]
+
+const url =
+  `mongodb+srv://tretiakowa2212:${password}@cluster0.racz2ff.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: newName,
+  number: newNumber,
+})
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person =>{ console.log(`${person.name} ${person.number}`)})
+    mongoose.connection.close()
+  })
+}
+
+if (process.argv.length > 3 && process.argv.length < 6) {
+  person.save().then(result => {
+    console.log(`added ${newName} number ${newNumber} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
