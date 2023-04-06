ecb3db70a4a7f5cdc8d3d62cfc74277e11e78194
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-04-06 14:13:28 +0600

    part4: exercise 4.8

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'first',
+    author: 'firstAu',
+    url: 'firstAu.com',
+    likes: 1
+  },
+  {
+    title: 'second',
+    author: 'secondAu',
+    url: 'secondAu.com',
+    likes: 2
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany()
+  let newBlog = new Blog(initialBlogs[0])
+  await newBlog.save()
+  newBlog = new Blog(initialBlogs[1])
+  await newBlog.save()
+})
+
+test('the amount of blog posts is ?', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.status).toBe(200)
+  expect(response.type).toBe('application\/json')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
