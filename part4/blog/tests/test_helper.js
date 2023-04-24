22f2a3dcc3ef761b9d232df8152591904e4c8464
Natalia Tretiakova <tretiakowa2212@gmail.com>
2023-04-24 18:07:49 +0600

    part4: exercise 4.15

--- part4/blog/tests/test_helper.js
+++ part4/blog/tests/test_helper.js
@@ -1,3 +1,4 @@
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -14,4 +15,9 @@ const initialBlogs = [
   }
 ]
 
-module.exports = {initialBlogs}
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
+module.exports = {initialBlogs, usersInDb}
